89ef202c6feea4005751d5ca27073a690f3591d7
Joseph Utecht <joseph.utecht@gmail.com>
2018-07-09 15:27:29 -0500

    better table formatting

--- conductor.py
+++ conductor.py
@@ -4,20 +4,6 @@ import os
 from jinja2 import Template
 import csv
 
-latex_jinja_env = jinja2.Environment(
-        block_start_string = '\BLOCK{',
-        block_end_string = '}',
-        variable_start_string = '\VAR{',
-        variable_end_string = '}',
-        comment_start_string = '\#{',
-        comment_end_string = '}',
-        line_statement_prefix = '%%',
-        line_comment_prefix = '%#',
-        trim_blocks = False,
-        autoescape = False,
-        loader = jinja2.FileSystemLoader(os.path.abspath('.'))
-)
-
 def clean_string(text):
     text = text.replace('&', '\\&')
     return text
@@ -41,11 +27,13 @@ def prep_authors(presentation, author_line):
             elif len(presentation['institutions']) == 2:
                 presentation['author_ss'] = '1,2'
     else:
+    # else process, co-authors
         while True:
             if(len(a) == 0):
                 break
             name = a.pop(0)
             name = name.replace(pre_name, '')
+            coauthor = {'name': name}
             if(len(a) > 0 and pre_institution in a[0]):
                 institution = a.pop(0)
                 institution = institution.replace(pre_institution, '')
@@ -57,25 +45,8 @@ def prep_authors(presentation, author_line):
                 if institution != 'Other':
                     if institution not in presentation['institutions']:
                         presentation['institutions'].append(institution)
-                    presentation['coauthors'].append({'name': name,
-                                                      'ss': presentation['institutions'].index(institution) + 1})
-                else:
-                    presentation['coauthors'].append({'name': name})
-            else:
-                presentation['coauthors'].append({'name': name})
-
-def render_poster_sessions(presentations):
-    template = latex_jinja_env.get_template('poster_template.tex')
-    even_presentations = list(filter(lambda x: x['number'] % 2 == 0, presentations))
-    odd_presentations = list(filter(lambda x: x['number'] % 2 == 1, presentations))
-    with open('latex/even_session.tex', 'w') as f:
-        f.write(template.render(presentations=even_presentations,
-                                session_times="12:15 pm  --  1:15 pm",
-                                session_name="Poster Session II / even number posters"))
-    with open('latex/odd_session.tex', 'w') as f:
-        f.write(template.render(presentations=odd_presentations,
-                                session_times="10:30 am  --  11:30 am",
-                                session_name="Post Session I / odd number posters"))
+                    coauthor['ss'] = presentation['institutions'].index(institution) + 1
+            presentation['coauthors'].append(coauthor)
 
 def prep_presentation(p, reg):
     p['author'] = reg['Abstract Author']
@@ -112,39 +83,65 @@ def prep_registration(reg):
         p['presenting'] = None
     return p
 
-registrations = []
-with open('registration.csv', 'r', encoding='utf-8-sig') as csvfile:
-    csvreader = csv.DictReader(csvfile, dialect='excel')
-    for row in csvreader:
-        registrations.append(row)
 
-attendees = []
-for registration in registrations:
-        attendees.append(prep_registration(registration))
+if __name__ == '__main__':
+    registrations = []
+    with open('registration.csv', 'r', encoding='utf-8-sig') as csvfile:
+        csvreader = csv.DictReader(csvfile, dialect='excel')
+        for row in csvreader:
+            registrations.append(row)
 
-poster_index = 1
-talk_index = 1
-for a in attendees:
-    if a.get('presenting', '') == 'P':
-        a['presentation_type'] = 'P'
-        a['number'] = poster_index
-        poster_index += 1
+    attendees = []
+    for registration in registrations:
+            attendees.append(prep_registration(registration))
 
-presentations = list(filter(lambda x: x['presenting'] == 'P', attendees))
-render_poster_sessions(presentations)
+    poster_index = 1
+    talk_index = 1
+    for a in attendees:
+        if a.get('presenting', '') == 'P':
+            a['presentation_type'] = 'P'
+            a['number'] = poster_index
+            poster_index += 1
+            
+    latex_jinja_env = jinja2.Environment(
+            block_start_string = '\BLOCK{',
+            block_end_string = '}',
+            variable_start_string = '\VAR{',
+            variable_end_string = '}',
+            comment_start_string = '\#{',
+            comment_end_string = '}',
+            line_statement_prefix = '%%',
+            line_comment_prefix = '%#',
+            trim_blocks = False,
+            autoescape = False,
+            loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+    )
 
-for a in attendees:
-    if a.get('presenting', '') == 'P':
-        a['activities'] = f"P{a['number']}, Poster"
-    elif a.get('presenting', '') == 'T':
-        a['activities'] = f"T{a['number']}, Oral"
-    elif a['reg_type'] == 'Undergraduate':
-        a['activities'] = 'Non-Presenter'
-    else:
-        a['activities'] = a['reg_type']
-        if a['judge']:
-            a['activities'] += "/Judge"
+    presentations = list(filter(lambda x: x['presenting'] == 'P', attendees))
+    template = latex_jinja_env.get_template('poster_template.tex')
+    even_presentations = list(filter(lambda x: x['number'] % 2 == 0, presentations))
+    odd_presentations = list(filter(lambda x: x['number'] % 2 == 1, presentations))
+    with open('latex/even_session.tex', 'w') as f:
+        f.write(template.render(presentations=even_presentations,
+                                session_times="12:15 pm  --  1:15 pm",
+                                session_name="Poster Session II / even number posters"))
+    with open('latex/odd_session.tex', 'w') as f:
+        f.write(template.render(presentations=odd_presentations,
+                                session_times="10:30 am  --  11:30 am",
+                                session_name="Post Session I / odd number posters"))
 
-template = latex_jinja_env.get_template('index_template.tex')
-with open('latex/index.tex', 'w') as f:
-    f.write(template.render(attendees=attendees))
+    for a in attendees:
+        if a.get('presenting', '') == 'P':
+            a['activities'] = f"P{a['number']}, Poster"
+        elif a.get('presenting', '') == 'T':
+            a['activities'] = f"T{a['number']}, Oral"
+        elif a['reg_type'] == 'Undergraduate':
+            a['activities'] = 'Non-Presenter'
+        else:
+            a['activities'] = a['reg_type']
+            if a['judge']:
+                a['activities'] += "/Judge"
+
+    template = latex_jinja_env.get_template('index_template.tex')
+    with open('latex/index.tex', 'w') as f:
+        f.write(template.render(attendees=attendees))

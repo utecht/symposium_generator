79124004d19b710ed3110dc905e13e0210bdea18
Joseph Utecht <joseph.utecht@gmail.com>
2018-07-09 11:01:47 -0500

    poster sessions mostly working

--- conductor.py
+++ conductor.py
@@ -18,6 +18,10 @@ latex_jinja_env = jinja2.Environment(
         loader = jinja2.FileSystemLoader(os.path.abspath('.'))
 )
 
+def clean_string(text):
+    text = text.replace('&', '\\&')
+    return text
+
 def prep_authors(presentation, author_line):
     pre_name = "Abstract Co-Author's Name: "
     pre_institution = "Co-Author's Institution: "
@@ -26,43 +30,70 @@ def prep_authors(presentation, author_line):
     a = author_line.split(', ')
 
     presentation['coauthors'] = []
-
-    while True:
-        if(len(a) == 0):
-            break
-        name = a.pop(0)
-        name = name.replace(pre_name, '')
-        if(len(a) > 0 and pre_institution in a[0]):
-            institution = a.pop(0)
-            institution = institution.replace(pre_institution, '')
-            if(institution == 'Other' and 
-                       len(a) > 0 and
-                       pre_other in a[0]):
+    # no co-authors, but second institution for primary author
+    if pre_institution in a[0]:
+        institution = a.pop()
+        institution = institution.replace(pre_institution, '')
+        if institution not in presentation['institutions']:
+            presentation['institutions'].append(institution)
+            if len(presentation['institutions']) == 1:
+                presentation['author_ss'] == '1'
+            elif len(presentation['institutions']) == 2:
+                presentation['author_ss'] == '1,2'
+    else:
+        while True:
+            if(len(a) == 0):
+                break
+            name = a.pop(0)
+            name = name.replace(pre_name, '')
+            if(len(a) > 0 and pre_institution in a[0]):
                 institution = a.pop(0)
-                institution = institution.replace(pre_other, '')
-        if institution != 'Other':
-            if institution not in presentation['institutions']:
-                presentation['institutions'].append(institution)
-            presentation['coauthors'].append({'name': name,
-                                              'ss': [presentation['institutions'].index(institution) + 1]})
-        else:
-            presentation['coauthors'].append({'name': name})
+                institution = institution.replace(pre_institution, '')
+                if(institution == 'Other' and 
+                           len(a) > 0 and
+                           pre_other in a[0]):
+                    institution = a.pop(0)
+                    institution = institution.replace(pre_other, '')
+                if institution != 'Other':
+                    if institution not in presentation['institutions']:
+                        presentation['institutions'].append(institution)
+                    presentation['coauthors'].append({'name': name,
+                                                      'ss': presentation['institutions'].index(institution) + 1})
+                else:
+                    presentation['coauthors'].append({'name': name})
+            else:
+                presentation['coauthors'].append({'name': name})
 
-def render_poster_session(presentations):
+def render_poster_sessions(presentations):
     template = latex_jinja_env.get_template('poster_template.tex')
-    print(template.render(presentations=presentations))
+    even_presentations = list(filter(lambda x: x['number'] % 2 == 0, presentations))
+    odd_presentations = list(filter(lambda x: x['number'] % 2 == 1, presentations))
+    with open('latex/even_session.tex', 'w') as f:
+        f.write(template.render(presentations=even_presentations,
+                                times="10:30 am  --  11:30 am",
+                                session_number="I",
+                                even_odd="even"))
+    with open('latex/odd_session.tex', 'w') as f:
+        f.write(template.render(presentations=odd_presentations,
+                                times="12:15 pm  --  1:15 pm",
+                                session_number="II",
+                                even_odd="odd"))
 
 def prep_registration(reg):
     p = {}
     p['presenter'] = reg['Name (First)'] + ' ' + reg['Name (Last)']
     p['author'] = reg['Abstract Author']
     p['author_ss'] = '1'
-    p['title'] = reg['Abstract Title']
+    p['title'] = clean_string(reg['Abstract Title'])
     institutions = [reg["Author's Institution"], reg["Author's Institution - Other"]]
-    if institutions[1] == "":
-        institutions.pop()
-    else:
-        p['author_ss'] = ['1', '2']
+    if '' in institutions:
+        institutions.remove('')
+    if 'Other' in institutions:
+        institutions.remove('Other')
+    if len(institutions) == 1:
+        p['author_ss'] == '1'
+    elif len(institutions) == 2:
+        p['author_ss'] == '1,2'
     p['institutions'] = institutions
     prep_authors(p, reg['Add Additional Authors'])
     return p
@@ -78,4 +109,7 @@ for registration in registrations:
     if(registration['Presenter'] == 'Yes'):
         presentations.append(prep_registration(registration))
 
-render_poster_session([p, p, p])
+for x, p in enumerate(presentations):
+    p['number'] = x + 1
+
+render_poster_sessions(presentations)
